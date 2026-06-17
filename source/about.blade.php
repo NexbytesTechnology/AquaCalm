e670ca0bb7be6acc32f97acd0414a63a008d997b
shubham maurya <131405109+shubhammauryainfo@users.noreply.github.com>
2026-06-17 23:49:54 +0530

    update

--- source/about.blade.php
+++ source/about.blade.php
@@ -16,10 +16,7 @@
         >
             More
         </button>
-        <div class="dropdown-menu" aria-labelledby="dropdownId">
-            <a class="dropdown-item" href="#">First Dropdown</a>
-            <a class="dropdown-item" href="#">Second Dropdown</a>
-        </div>
+      
     </div>
 </div>
 
